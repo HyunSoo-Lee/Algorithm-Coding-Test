cd4e799fd2062e4cb6e494d5018ecb7faa8875fd
HyunSoo Lee <oceanyum@gmail.com>
2024-02-13 22:02:01 +0800

    마라톤 효율성 체크

--- marathon.py
+++ marathon.py
@@ -9,12 +9,19 @@
 participant = ["marina", "josipa", "nikola", "vinko", "filipa"]
 completion = ["josipa", "filipa", "marina", "nikola"]
 
+# 접근 방법
+# participant 리스트를 딕셔너리화
+# completion 리스트와 비교
+# failure 리스트를 만들어야 하나요?
+
+
 def solution(participant, completion):
     # dictionary comprehension
     dict_participant = {i:participant[i] for i in range(len(participant))}
     print(dict_participant)
     
     for i in range(len(completion)):
+        # O(n^2) 발생 지점. 고쳐야 함
         failure = [k for k,v in dict_participant.items() if v == completion[i]]
         dict_participant.pop(failure[0])
     answer = list(dict_participant.values())
