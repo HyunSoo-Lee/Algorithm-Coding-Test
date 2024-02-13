5d3eaefc58ec69ee677e39fc8b719c5af4bbd12b
HyunSoo Lee <oceanyum@gmail.com>
2024-02-13 16:42:33 +0800

    comprehension

--- marathon.py
+++ marathon.py
@@ -10,16 +10,15 @@ participant = ["marina", "josipa", "nikola", "vinko", "filipa"]
 completion = ["josipa", "filipa", "marina", "nikola"]
 
 def solution(participant, completion):
-    # list comprehension
-    dict_participant = {i:participant[i] for i in range(len(participant))}
-
     # dictionary comprehension
+    dict_participant = {i:participant[i] for i in range(len(participant))}
+    print(dict_participant)
+    
     for i in range(len(completion)):
         failure = [k for k,v in dict_participant.items() if v == completion[i]]
         dict_participant.pop(failure[0])
-
-    for v in dict_participant.values():
-        answer = v
-    return answer
+    answer = list(dict_participant.values())
+    return answer[0]
+    
 
 print(solution(participant,completion))
