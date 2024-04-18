8eb017b4235b7f942d943bd63331b3423a0bac8d
HyunSoo Lee <oceanyum@gmail.com>
2024-04-18 23:24:15 +0900

    우선순위 큐

--- queue_process.py
+++ queue_process.py
@@ -6,14 +6,23 @@ def solution(priorities, location):
     prior_process = priorities[0]
     i = 0
     while(1):
-        print(prior_process)
+        print(priorities[i], i)
         if priorities[i] > prior_process:
             prior_process = priorities[i]
             priorities.remove(priorities[i])
-        else: i += 1
-        print(priorities, i)
+            answer += 1
+        elif i < len(priorities) - 1: i += 1
+        else : 
+            if i != location:
+                # 여기서부터 다시 시작, len(priorities)보다 적으면 끝까지 돌리고, 아니면 다시 i = 0으로 돌아가야해.
+                i = 0
+                prior_process = priorities[0]
+            elif i == location:
+                print(answer)
+                quit()
+        print(priorities, prior_process)
     return answer
 
-priorities = [2,1,3,2] #[1,1,9,1,1,1]
-location = 2 #0
+priorities = [1,1,9,1,1,1] #[2,1,3,2]
+location = 0 #2
 print(solution(priorities, location))
