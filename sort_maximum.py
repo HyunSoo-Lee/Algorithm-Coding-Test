9d758a474ce83f1b002d66b5f7c42ec70ed96f05
ocean <oceanyum@gmail.com>
2024-08-12 14:41:41 +0900

    코테 복귀전

---
+++ sort_maximum.py
@@ -0,0 +1,10 @@
+# problem URL
+# https://school.programmers.co.kr/learn/courses/30/lessons/42746
+
+def solution(numbers):
+    answer = 0
+    # 뭔가 재귀함수같다... 그죠?
+    return answer
+
+
+numbers = [6,10,2]
