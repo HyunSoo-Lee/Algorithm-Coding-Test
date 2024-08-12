e51d0e380f12f0925f93aae9c37c05a9856915b7
ocean <oceanyum@gmail.com>
2024-08-12 15:14:21 +0900

    practice 8/12

--- sort_maximum.py
+++ sort_maximum.py
@@ -2,9 +2,18 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42746
 
 def solution(numbers):
-    answer = 0
-    # 뭔가 재귀함수같다... 그죠?
-    return answer
+    #answer = 0
+    n = len(numbers)
+    print(numbers)
+    for i in range (n):
+        for j in range(n - i - 1):
+            print(i, j, n)
+            if numbers[i] > numbers[i+1]:
+                numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
+                print(numbers)
+    return numbers
+    #return answer
 
 
 numbers = [6,10,2]
+print(solution(numbers))
