74a4aa8c2bf2159c6d4993fdada8457d1642d331
ocean <oceanyum@gmail.com>
2024-08-13 18:36:27 +0900

    두자리수 이상 숫자에 대한 비교 솔루션

--- sort_maximum.py
+++ sort_maximum.py
@@ -1,19 +1,31 @@
 # problem URL
 # https://school.programmers.co.kr/learn/courses/30/lessons/42746
+def comp(x, y):
+    if (int(x + y) > int(y + x)):
+        return True
+    else:
+        return False
+
 
 def solution(numbers):
+    
     #answer = 0
     n = len(numbers)
     print(numbers)
     for i in range (n-1):
         for j in range(n-i-1):
-            print('i = ' + str(i) + ' j = ' + str(j) + ' n = ' + str(n))
+            print('비교 대상',numbers[j],numbers[j+1])
             if numbers[j] > numbers[j+1]:
                 numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
                 print(numbers)
-    return numbers
+    
+    numbers = [str(n) for n in numbers]
+    result = ''.join(reversed(numbers))
+    return result
     #return answer
 
+#and comp(numbers[j], numbers[j+1])
 
-numbers = [3,30,34,5,9]
+#numbers = [3,30,34,5,9]
+numbers = [6, 10, 2]
 print(solution(numbers))
