61865637d6f40cf7f42b436710e0dfe5b9534d1f
ocean <oceanyum@gmail.com>
2024-08-13 17:53:57 +0900

    버블 정렬 구현

--- sort_maximum.py
+++ sort_maximum.py
@@ -5,15 +5,15 @@ def solution(numbers):
     #answer = 0
     n = len(numbers)
     print(numbers)
-    for i in range (n):
-        for j in range(n - i - 1):
-            print(i, j, n)
-            if numbers[i] > numbers[i+1]:
-                numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
+    for i in range (n-1):
+        for j in range(n-i-1):
+            print('i = ' + str(i) + ' j = ' + str(j) + ' n = ' + str(n))
+            if numbers[j] > numbers[j+1]:
+                numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
                 print(numbers)
     return numbers
     #return answer
 
 
-numbers = [6,10,2]
+numbers = [3,30,34,5,9]
 print(solution(numbers))
