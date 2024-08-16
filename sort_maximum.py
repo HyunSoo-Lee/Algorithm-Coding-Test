172983b1dfba7ec361668c7e8cde44f5d5e00cc4
ocean <oceanyum@gmail.com>
2024-08-16 17:58:32 +0900

    테스트 케이스 수정

--- sort_maximum.py
+++ sort_maximum.py
@@ -1,8 +1,8 @@
 # problem URL
 # https://school.programmers.co.kr/learn/courses/30/lessons/42746
 def comp(x, y):
-    print(int(str(x) + str(y)))
-    print(int(str(y) + str(x)))
+    # print(int(str(x) + str(y)))
+    # print(int(str(y) + str(x)))
     if (int(str(x) + str(y)) > int(str(y) + str(x))):
         return False
     else:
@@ -12,18 +12,21 @@ def comp(x, y):
 def solution(numbers): 
     #answer = 0
     n = len(numbers)
-    print(numbers)
+    #print(numbers)
     for i in range (n-1):
+        swapped = False
         for j in range(n-i-1):
-            print('비교 대상',numbers[j],numbers[j+1])
+            #print('비교 대상',numbers[j],numbers[j+1])
             #if numbers[j] > numbers[j+1] and comp(numbers[j], numbers[j+1]):
             if comp(numbers[j], numbers[j+1]):
                 numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
-                print(numbers)
-    
+                #print(numbers)
+                swapped = True
+        if not swapped:
+            break
     numbers = [str(n) for n in numbers]
     result = ''.join(numbers)
-    return result
+    return result if result[0] != '0' else '0'
     #return answer
 
 #and comp(numbers[j], numbers[j+1])
