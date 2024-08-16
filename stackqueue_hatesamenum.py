7d3955d1b03e1ca50616a6c7c87c706b84826e45
ocean <oceanyum@gmail.com>
2024-08-16 18:08:25 +0900

    문제 해결

--- stackqueue_hatesamenum.py
+++ stackqueue_hatesamenum.py
@@ -3,9 +3,11 @@ arr = [1,1,3,3,0,1,1]
 def solution(arr):
     answer = []
     for i in range(len(arr)):
-        # if answer empty
-        if answer[-1] == arr[i]:
+        if not answer:
             answer.append(arr[i])
+        elif answer[-1] != arr[i]:
+            answer.append(arr[i])
+
     return answer
 
-print(solution)
+print(solution(arr))
