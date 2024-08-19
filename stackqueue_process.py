c2ece18f273f67529ff05f29085f6a8074c6a63f
ocean <oceanyum@gmail.com>
2024-08-19 15:18:41 +0900

    문제 해결~

---
+++ stackqueue_process.py
@@ -0,0 +1,26 @@
+# 스택/큐 > 프로세스
+# problem URL
+# https://school.programmers.co.kr/learn/courses/30/lessons/42587
+
+def solution(priorities, location):
+    order = []
+    pointer = 0
+    while True:
+        if max(priorities) == 0:
+            break
+        else: 
+            #print(pointer, len(priorities),priorities[pointer])
+            if priorities[pointer] == max(priorities):
+                priorities[pointer] = 0
+                order.append(pointer)
+                #print(priorities)
+            if pointer == len(priorities) - 1:
+                pointer = 0
+            else: pointer += 1
+    
+    answer = order.index(location) + 1
+    return answer
+
+priorities = [1, 1, 9, 1, 1, 1]	
+location = 0
+print(solution(priorities, location))
