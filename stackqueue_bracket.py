d0031fc776dbe47b2b04a6ebd829efc4eda7cc59
ocean <oceanyum@gmail.com>
2024-08-19 13:39:46 +0900

    문제 해결~

--- stackqueue_bracket.py
+++ stackqueue_bracket.py
@@ -3,18 +3,19 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12909
 
 def solution(s):
-    if '(' not in s:
-        return False
     stk = []
-    for i in range (len(s)):
+    for i in range(len(s)):
         if s[i] == '(':
             stk.append(s[i])
         else:
             if stk:
                 stk.pop()
+            else:
+                return False
     if len(stk) == 0:
         return True
     else:
-        return False
+        return False  # 명시적으로 False를 반환
+
 
 print(solution(""))
