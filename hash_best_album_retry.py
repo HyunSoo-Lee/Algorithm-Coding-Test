76090b4f3dbf8c42b7066f110e5406a8c1af598f
ocean <oceanyum@gmail.com>
2024-08-22 16:43:40 +0900

    22일 수정

--- hash_best_album_retry.py
+++ hash_best_album_retry.py
@@ -7,24 +7,20 @@ plays = [500, 600, 150, 800, 2500, 800]
 
 def solution(genres, plays):
     answer = []
-
-    dict_genres = {}
-    i = 0
-    for k in genres:
-        if k in dict_genres:
-            dict_genres[k].append(plays[i])
-        else:
-            dict_genres[k] = [plays[i]]
-        i += 1
-    print(dict_genres)
-
+    g_dict = {}
+    max_p = 0
+    # zip함수는 여러 이터러블(리스트, 튜플 등)을 병렬로 묶어준다.
+    # enumerate 함수는 이터러블을 입력받아, 해당 (인덱스,원소) 형태로 바꾸어준다.
+    for i, (g, p) in enumerate(zip(genres,plays)):
+            print(f"인덱스 : {i}, 장르 : {g}, 횟수  : {p}")
+            if g not in g_dict:
+                  g_dict[g] = []
+            g_dict[g].append([p,i])
+            if p >= max_p:
+                max_p = p
+                answer
+    print(g_dict, max_p)
     # 속한 노래가 많이 재생된 장르를 먼저 수록
-    print(dict_genres.items())
-    list_dict = list(dict_genres.items())
-    print(list_dict)
-    for k,v in list_dict:
-        print(max(v))
-
     # 장르 내에서 많이 재생된 노래를 먼저 수록
     # 장르 내에서 재생 횟수가 같은 노래중에서는 고유 번호가 낮은 노래를 먼저 수록
     return answer
