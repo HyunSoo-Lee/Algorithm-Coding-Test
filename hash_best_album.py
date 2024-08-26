d97a89cbe599833607fa9a940a93e004a67bf0ee
ocean <oceanyum@gmail.com>
2024-08-26 13:53:21 +0900

    8/26

--- hash_best_album.py
+++ hash_best_album.py
@@ -2,50 +2,41 @@
 # problem URL
 # https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=python
 
-genres = ["classic", "pop", "classic", "classic", "pop", "classic"]
-plays = [500, 600, 150, 800, 2500, 800]
+genres = ["classic", "pop", "classic", "classic", "pop"]
+plays = [500, 600, 150, 800, 2500]
 
 def solution(genres, plays):
     answer = []
-    music_dict = {}
-    # 장르 묶어서 딕셔너리 만들어주기
-    for i in range(len(genres)):
-        song = [plays[i],i]
-        if genres[i] in music_dict:
-            music_dict[genres[i]].append(song)
-        else:
-            music_dict[genres[i]] = [song]
+    g_dict = {}
+    # zip함수는 여러 이터러블(리스트, 튜플 등)을 병렬로 묶어준다.
+    # enumerate 함수는 이터러블을 입력받아, 해당 (인덱스,원소) 형태로 바꾸어준다.
+    # 장르 : [[재생회수,고유번호], [재생회수,고유번호]...] 형태의 딕셔너리로 전환
+    for i, (g, p) in enumerate(zip(genres,plays)):
+            #print(f"인덱스 : {i}, 장르 : {g}, 횟수  : {p}")
+            if g not in g_dict:
+                  g_dict[g] = []
+            g_dict[g].append([p,i])
 
-    #장르 내 플레이 횟수 맞춰서 소팅해주기
-    for genres, songs in music_dict.items():
-        music_dict[genres] = sorted(songs, key=lambda x: x[0])
-    print(music_dict)
-    
-    # while문 돌리면서 확인해야 하는것 : 
-    # 가장 큰 값을 가진 장르에서 두개 빼오고 지우기 반복
-    while music_dict:
-        # print(music_dict)
-        max_genre = max(music_dict, key = lambda x : x[-1][0])
-        # print(max_genre)
-        # print(music_dict[max_genre])
-        # print(music_dict[max_genre][-1][0])
-        if len(music_dict[max_genre]) > 2:
-            if music_dict[max_genre][-1][0] == music_dict[max_genre][-2][0]:
-                answer.append(music_dict[max_genre][-2][1])
-                answer.append(music_dict[max_genre][-1][1])    
-            else:
-                answer.append(music_dict[max_genre][-1][1])
-                answer.append(music_dict[max_genre][-2][1])
-        elif len(music_dict[max_genre]) == 2:
-            if music_dict[max_genre][1][0] == music_dict[max_genre][0][0]:
-                answer.append(music_dict[max_genre][0][1])
-                answer.append(music_dict[max_genre][1][1])
-            else:
-                answer.append(music_dict[max_genre][1][1])
-                answer.append(music_dict[max_genre][0][1])
-        elif len(music_dict[max_genre]) == 1:
-            answer.append(music_dict[max_genre][0][1])
-        del music_dict[max_genre]
+    #print(g_dict)
+    # 속한 노래가 많이 재생된 장르를 먼저 수록
+    # 딕셔너리를 재생순으로 재정렬
+    for key in g_dict:
+        g_dict[key].sort(key=lambda x: x[0], reverse=True)
+    #print(g_dict)
+    #가장 큰 재생회수를 가진 장르 순으로 리스트
+    g_sort = []
+    p_max = 0
+    for key in g_dict:
+        if g_dict[key][0][0] > p_max:
+            g_sort.append(key)
+            p_max = g_dict[key][0][0]
+    g_sort.reverse()
+    #print(g_sort, p_max)
+    # 장르 내에서 많이 재생된 노래를 먼저 수록
+    for g in g_sort:
+        answer.append(g_dict[g][0][1])
+        answer.append(g_dict[g][1][1])
+    # 장르 내에서 재생 횟수가 같은 노래중에서는 고유 번호가 낮은 노래를 먼저 수록
     return answer
 
 print(solution(genres,plays))
---
+++ stackqueue_bridge.py
@@ -0,0 +1,5 @@
+# 스택/큐 > 다리를 지나는 트럭
+# problem URL
+# https://school.programmers.co.kr/learn/courses/30/lessons/42583
+
+
